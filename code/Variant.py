1ff8a587ddaa5e9722b413f306b6ceac32aeea23
dytk2134 <dytk2134@gmail.com>
2016-10-31 09:23:59 +0800

    update

--- code/Variant.py
+++ code/Variant.py
@@ -27,6 +27,21 @@ def allowed_file(filename):
             filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
+"""get allele frequency from database"""
+def get_freq(con,table,column,poplen,chr,pos,ref,alt):
+    """Connect to MySQL database"""
+    freq_result = []
+    sql_command="SELECT %s FROM allele_frequency.%s WHERE (chr =\"%s\") AND (pos=\"%s\") AND (ref = \"%s\") AND (alt = \"%s\")" % (column,table,chr,pos,ref,alt)
+    con.execute(sql_command)
+    result = con.fetchall()
+    if len(result) > 0:
+        freq_result.extend(list(result[0]))
+    else:
+        from itertools import repeat
+        freq_result.extend(repeat(".",poplen))
+    return freq_result
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -136,132 +151,133 @@ def search():
 @app.route('/vcf', methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
-        import time
-        tStart = time.time()
+
         """Connect to MySQL database"""
         db=MySQLdb.connect(host="localhost",user="user",passwd="bioinfo")
         con=db.cursor()
+        """user input information"""
+        Output_format = request.form["format"]
+        Genomes_population = request.form.getlist("Genomes")#1000 Genomes population list
+        JPN_population = request.form.getlist("JPN")
+        ESP_population = request.form.getlist("ESP")
+
+        Final_result = [] #存最後的結果
+        Final_result_title = ["chr","pos","ref","alt"]
+        if len(Genomes_population)!=0:
+            Genomes_population_column = ",".join(list(map(lambda orig_string:orig_string + "_Ref_" + Output_format +","+orig_string + "_Alt_"+ Output_format,Genomes_population)))
+            Final_result_title.extend(Genomes_population_column.split(","))
+        if "1KJPN" in JPN_population:
+            JPN_1_column = "1KJPN_Ref_" + Output_format + "," + "1KJPN_Alt_" +Output_format
+            Final_result_title.extend(JPN_1_column.split(","))
+        if "2KJPN" in JPN_population:
+            JPN_2_column = "2KJPN_Ref_" + Output_format + "," + "2KJPN_Alt_" +Output_format
+            Final_result_title.extend(JPN_2_column.split(","))
+        if len(ESP_population) !=0:
+            ESP_population_column = ",".join(list(map(lambda orig_string:orig_string + "_Ref_" +Output_format +","+orig_string + "_Alt_" +Output_format,ESP_population)))
+            Final_result_title.extend(ESP_population_column.split(","))
+        if len(request.form.getlist("REVEL")) != 0:
+            Final_result_title.extend(["aaref","aaalt","REVEL"])
+        Final_result_title.extend(["gene_name","description","FPKM(Heart muscle)","Ranking/Total"])
+
+        Result_line = 0
+
+
+        #if user input file
         file = request.files['file']
+        #如果使用者上傳檔案，則使用此檔案的資訊
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename =secure_filename(file.filename)
+            variant_text = multiple_replace(request.form['variants_list'],{"\"":"","\'":"","`":"","%":""})
+            variant_text = variant_text.upper()
+            variant_list =list(filter(None,re.split(regexPattern,variant_text)))
+            vartext_file = open(os.path.join(app.config['UPLOAD_FOLDER'],"user_input.avinput"),"w")
 
+            for var in variant_list:
+                vars = re.compile("([\dXYM]+):(\d+)([ATCG]+)>([ATCG]+)").split(var)
+                if len(vars)<6:
+                    Final_result.append([var[0],".",".","."])
+                    vartext_file.write(var[0]+"\t.\t.\t.\n")
+                else:
+                    Final_result.append(vars[1:5])
+                    vartext_file.write("chr"+vars[1]+"\t"+vars[2]+"\t"+str(int(vars[2])+len(vars[3])-1)+"\t"+vars[3]+"\t"+vars[4]+"\n")
+
+
+                if len(Genomes_population)!=0:
+                    Final_result[Result_line].extend(get_freq(con,"1000Genomes_5pop_"+Output_format,Genomes_population_column,len(Genomes_population)*2,vars[1],vars[2],vars[3],vars[4]))
+                if "1KJPN" in JPN_population:
+                    Final_result[Result_line].extend(get_freq(con,"1KJPN_"+Output_format,JPN_1_column,2,vars[1],vars[2],vars[3],vars[4]))
+                if "2KJPN" in JPN_population:
+                    Final_result[Result_line].extend(get_freq(con,"2KJPN_"+Output_format,JPN_2_column,2,vars[1],vars[2],vars[3],vars[4]))
+                if len(ESP_population) !=0:
+                    Final_result[Result_line].extend(get_freq(con,"ESP_"+Output_format,ESP_population_column,len(ESP_population)*2,vars[1],vars[2],vars[3],vars[4]))
+
+                if len(request.form.getlist("REVEL"))!=0:
+                    Final_result[Result_line].extend(get_freq(con,"REVEL","aaref,aaalt,REVEL",3,vars[1],vars[2],vars[3],vars[4]))
+                sql_command = "SELECT `gene_name`, `description`,`FPKM(heart muscle)`,`Ranking/Total` FROM Heart_gene_expression.Proteinaltas_RNA WHERE chr = \"%s\" AND %s BETWEEN Proteinaltas_RNA.start AND Proteinaltas_RNA.end" % (vars[1],vars[2])
+                print(sql_command)
+                con.execute(sql_command)
+                RNA_result = con.fetchall()
+                if len(RNA_result) > 0:
+                    Final_result[Result_line].extend(list(RNA_result[0]))
+                else:
+                    from itertools import repeat
+                    Final_result[Result_line].extend(repeat(".",4))
+                Result_line +=1
+            vartext_file.close()
+
+        elif file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            ref_ver = request.form["reference"]#GRCh38 or GRCh37
-            Output_format = request.form["format"]
-            Genomes_population = request.form.getlist("Genomes")#1000 Genomes population list
-            JPN_population = request.form.getlist("JPN")
-            ESP_population = request.form.getlist("ESP")
-            Final_result = [] #存最後的結果
-            Final_result_title = ["chr","pos","ref","alt"]
-            if len(Genomes_population)!=0:
-
-                Genomes_population_column = ",".join(list(map(lambda orig_string: orig_string + "_Ref_" + Output_format +","+ orig_string + "_Alt_" + Output_format,Genomes_population)))
-                Final_result_title.extend(Genomes_population_column.split(","))
-            if "1KJPN" in JPN_population:
-
-                JPN_1_column = "1KJPN_Ref_" + Output_format + "," + "1KJPN_Alt_" + Output_format
-                Final_result_title.extend(JPN_1_column.split(","))
-            if "2KJPN" in JPN_population:
-
-                JPN_2_column = "2KJPN_Ref_" + Output_format + "," + "2KJPN_Alt_" + Output_format
-                Final_result_title.extend(JPN_2_column.split(","))
-            if len(ESP_population) !=0:
-
-                ESP_population_column = ",".join(list(map(lambda orig_string: orig_string + "_Ref_" + Output_format +","+ orig_string + "_Alt_" + Output_format,ESP_population)))
-                Final_result_title.extend(ESP_population_column.split(","))
-            Final_result_title.extend(["gene_name(ensembl)","FPKM(Heart muscle)","RNA_Level","Protein_Level","Reliability"])
-
-
-            Result_line = 0
             vcf_reader = vcf.Reader(open(os.path.join(app.config['UPLOAD_FOLDER'],filename)))
             for record in vcf_reader:
                 for idx in range(0,len(record.ALT)):
                     Final_result.append([record.CHROM,record.POS,record.REF,record.ALT[idx]])
                     if len(Genomes_population)!=0:
-                        sql_command="SELECT %s FROM allele_frequency.1000Genomes_%s WHERE (chr =\"%s\") AND (pos =\"%s\") AND (REF = \"%s\") AND (ALT = \"%s\")" % (Genomes_population_column,Output_format,record.CHROM,record.POS,record.REF,record.ALT[idx])
-                        con.execute(sql_command)
-                        Genomes_result = con.fetchall()
-                        if len(Genomes_result) > 0:
-                            Final_result[Result_line].extend(list(Genomes_result[0]))
-                        else:
-                            from itertools import repeat
-                            Final_result[Result_line].extend(repeat(".",len(Genomes_population)*2))
+                        Final_result[Result_line].extend(get_freq(con,"1000Genomes_5pop_"+Output_format,Genomes_population_column,len(Genomes_population)*2,record.CHROM,record.POS,record.REF,record.ALT[idx]))
 
                     if "1KJPN" in JPN_population:
-                        sql_command="SELECT %s FROM allele_frequency.1KJPN_%s WHERE (chr =\"%s\") AND (pos =\"%s\") AND (REF = \"%s\") AND (ALT = \"%s\")" % (JPN_1_column,Output_format,record.CHROM,record.POS,record.REF,record.ALT[idx])
-                        con.execute(sql_command)
-                        JPN_1_result = con.fetchall()
-                        if len(JPN_1_result) > 0:
-                            Final_result[Result_line].extend(list(JPN_1_result[0]))
-                        else:
-                            from itertools import repeat
-                            Final_result[Result_line].extend(repeat(".", 2))
-
+                        Final_result[Result_line].extend(get_freq(con,"1KJPN_"+Output_format,JPN_1_column,2,record.CHROM,record.POS,record.REF,record.ALT[idx]))
                     if "2KJPN" in JPN_population:
-                        sql_command="SELECT %s FROM allele_frequency.2KJPN_%s WHERE (chr =\"%s\") AND (pos =\"%s\") AND (REF = \"%s\") AND (ALT = \"%s\")" % (JPN_2_column,Output_format,record.CHROM,record.POS,record.REF,record.ALT[idx])
-                        con.execute(sql_command)
-                        JPN_2_result = con.fetchall()
-                        if len(JPN_2_result) > 0:
-                            Final_result[Result_line].extend(list(JPN_2_result[0]))
-                        else:
-                            from itertools import repeat
-                            Final_result[Result_line].extend(repeat(".", 2))
-
+                       Final_result[Result_line].extend(get_freq(con,"2KJPN_"+Output_format,JPN_2_column,2,record.CHROM,record.POS,record.REF,record.ALT[idx]))
                     if len(ESP_population) !=0:
-                         sql_command="SELECT %s FROM allele_frequency.ESP_%s WHERE (chr =\"%s\") AND (pos =\"%s\") AND (REF = \"%s\") AND (ALT = \"%s\")" % (ESP_population_column,Output_format,record.CHROM,record.POS,record.REF,record.ALT[idx])
-                         con.execute(sql_command)
-                         ESP_result = con.fetchall()
-                         if len(ESP_result) > 0:
-                             Final_result[Result_line].extend(list(ESP_result[0]))
-                         else:
-                             from itertools import repeat
-                             Final_result[Result_line].extend(repeat(".",len(ESP_population)*2))
-                         #RNA expression
-                    sql_command="SELECT %s FROM Heart_gene_expression.Ensembl_GRCh37_RNA WHERE chr = \"%s\" AND %s BETWEEN gene_start AND gene_end" % ("`gene_name(ensembl)`,`FPKM`,`RNA_Level`",record.CHROM,record.POS)
+                        Final_result[Result_line].extend(get_freq(con,"ESP_"+Output_format,ESP_population_column,len(ESP_population)*2,record.CHROM,record.POS,record.REF,record.ALT[idx]))
+                    if len(request.form.getlist("REVEL"))!=0:
+                        Final_result[Result_line].extend(get_freq(con,"REVEL","aaref,aaalt,REVEL",3,record.CHROM,record.POS,record.REF,record.ALT[idx]))
+                       #RNA expression
+                    sql_command = "SELECT `gene_name`, `description`,`FPKM(heart muscle)`,`Ranking/Total` FROM Heart_gene_expression.Proteinaltas_RNA WHERE chr = \"%s\" AND %s BETWEEN Proteinaltas_RNA.start AND Proteinaltas_RNA.end" % (record.CHROM,record.POS)
                     con.execute(sql_command)
                     RNA_result = con.fetchall()
                     if len(RNA_result) > 0:
                         Final_result[Result_line].extend(list(RNA_result[0]))
                     else:
                         from itertools import repeat
-                        Final_result[Result_line].extend(repeat(".",3))
+                        Final_result[Result_line].extend(repeat(".",4))
 
-
-		    #Protein expression
-                    sql_command="SELECT %s FROM Heart_gene_expression.Ensembl_GRCh37_Protein WHERE chr = \"%s\" AND %s BETWEEN gene_start AND gene_end" % ("`Protein_Level`,`Reliability`",record.CHROM,record.POS)
-                    con.execute(sql_command)
-                    Protein_result = con.fetchall()
-                    if len(Protein_result) > 0:
-                        Final_result[Result_line].extend(list(Protein_result[0]))
-                    else:
-                        from itertools import repeat
-                        Final_result[Result_line].extend(repeat(".",2))
                     Result_line +=1
+        print(Final_result)
 
 #ANNOVAR
-            import subprocess
+        import subprocess
+        if file.filename == '':
+            cmd='/home/bioinfo/Heart_gene_database/HeartInter/code/tool/annovar/table_annovar.pl %s /home/bioinfo/Heart_gene_database/HeartInter/code/tool/annovar/humandb/ -buildver hg19 -remove -protocol refGene,gerp++gt2 -operation g,f -nastring . --outfile %s' % (os.path.join(app.config['UPLOAD_FOLDER'],"user_input.avinput"),os.path.join(app.config['UPLOAD_FOLDER'],"output"))
+            print(cmd)
+        else:
             cmd='/home/bioinfo/Heart_gene_database/HeartInter/code/tool/annovar/table_annovar.pl %s /home/bioinfo/Heart_gene_database/HeartInter/code/tool/annovar/humandb/ -buildver hg19 -remove -protocol refGene,gerp++gt2 -operation g,f -nastring . -vcfinput --outfile %s' % (os.path.join(app.config['UPLOAD_FOLDER'],filename),os.path.join(app.config['UPLOAD_FOLDER'],"output"))
 
-            retcode = subprocess.call(cmd, shell=True)
-            if retcode != 0: sys.exit(retcode)
-            Current_line = 0
-            Final_result_title.extend(["Func.refGene","ExonicFunc.refGene","AAChange.refGene","gerp++gt2"])
-            with open(os.path.join(app.config['UPLOAD_FOLDER'],"output.hg19_multianno.txt"),"r") as annovar:
-                for line in annovar:
-                    Current_line+=1
-                    line = line.strip()
-                    if Current_line !=1:
-                        lines = line.split("\t")
-                        Final_result[Current_line-2].extend([lines[5],lines[8],lines[9],lines[10]])
-            tEnd = time.time()
-            print ("It cost %f sec" % (tEnd - tStart))
+        retcode = subprocess.call(cmd, shell=True)
+        if retcode != 0: sys.exit(retcode)
+        Current_line = 0
+        Final_result_title.extend(["Func.refGene","ExonicFunc.refGene","AAChange.refGene","gerp++gt2"])
+        with open(os.path.join(app.config['UPLOAD_FOLDER'],"output.hg19_multianno.txt"),"r") as annovar:
+            for line in annovar:
+                Current_line+=1
+                line = line.strip()
+                if Current_line !=1:
+                    lines = line.split("\t")
+                    Final_result[Current_line-2].extend([lines[5],lines[8],lines[9],lines[10]])
 
-            db.close()
+        db.close()
             #os.remove(os.path.join(app.config['UPLOADED_ITEMS_DEST'], filename))
-            return render_template("Teresult.html",results = Final_result,keys = Final_result_title)
+        return render_template("Teresult.html",results = Final_result,keys = Final_result_title)
     return render_template('vcf.html')
 
 
