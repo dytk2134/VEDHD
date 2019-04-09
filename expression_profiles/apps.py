53c67aacf520c8618e145308995a13c6db1becee
dytk2134 <dytk2134@gmail.com>
2019-04-09 16:47:08 +0800

    initial project

---
+++ expression_profiles/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExpressionProfilesConfig(AppConfig):
+    name = 'expression_profiles'
---
+++ variants_search/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VariantsSearchConfig(AppConfig):
+    name = 'variants_search'
